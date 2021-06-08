d111e3e882e46f80c4a479529a832f1bff3d624b
島　浩文 <hshima@straw-hat.jp>
2021-06-09 08:47:04 +0900

    api/userを追加

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,3 +1,31 @@
 <template>
+ <div class="container">
   <h1>Admin Page!!</h1>
+  <div>
+    <button id="api" @click="callApi">Call API</button>
+    <div>
+      Message From Api: <strong>{{ message }}</strong>
+    </div>
+  </div>
+ </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    message: ''
+  }),
+  methods: {
+    async callApi () {
+      try {
+        const res = await axios.get("/api/user");
+        this.message = res.data.text;
+        console.log(message);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+</script>
